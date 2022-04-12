100e5681cfbc2f3e068002a0d1cfe20f481b87da
YutoKajita <k021g1149@g.neec.ac.jp>
2022-04-12 16:14:06 +0900

    01_02_確認課題

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -51,27 +51,4 @@ class GameScene {
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
-	 
-	// テクスチャハンドル
-	uint32_t textureHandle_ = 0;
-
-	//サウンドデータハンドル
-	uint32_t soundDataHandle_ = 0;
-
-	//音声再生ハンドル
-	uint32_t voiceHandle_ = 0;
-
-	//3Dモデル
-	Model* model_ = nullptr;
-
-	//スプライト
-	Sprite* sprite_ = nullptr;
-
-	//ワールドトランスフォーム
-	WorldTransform worldTransform_;
-	//ビュープロジェクション
-	ViewProjection viewProjection_;
-
-	//値を表示したい変数
-	int32_t value_ = 0;
 };
