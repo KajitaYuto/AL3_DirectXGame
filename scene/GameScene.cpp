2039a86a91e6ec472db9a9c31e2eb1e38525b912
YutoKajita <k021g1149@g.neec.ac.jp>
2022-04-19 14:28:55 +0900

    02_01_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -23,14 +23,38 @@ void GameScene::Initialize() {
 	//3Dモデルの生成
 	model_ = Model::Create();
 
+	//X,Y,Z方向のスケーリングを設定
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+	// X,Y,Z方向のスケーリングを設定
+	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
+	// X,Y,Z軸周りの平行移動を設定
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+
 	//ワールドトランスフォームの初期化
 	worldTransform_.Initialize();
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 }
 
-void GameScene::Update() {
+void GameScene::Update() { 
+	std::string strDebug1 = std::string("translation:(") +
+	std::to_string(worldTransform_.translation_.x) + std::string(",") +
+	std::to_string(worldTransform_.translation_.y) + std::string(",") +
+	std::to_string(worldTransform_.translation_.z) + std::string(")");
 
+	std::string strDebug2 = std::string("rotation:(") +
+	std::to_string(worldTransform_.rotation_.x) + std::string(",") +
+	std::to_string(worldTransform_.rotation_.y) + std::string(",") +
+	std::to_string(worldTransform_.rotation_.z) + std::string(")");
+
+	std::string strDebug3 = std::string("scale:(") +
+	std::to_string(worldTransform_.scale_.x) + std::string(",") +
+	std::to_string(worldTransform_.scale_.y) + std::string(",") +
+	std::to_string(worldTransform_.scale_.z) + std::string(")");
+
+	debugText_->Print(strDebug1, 50, 50, 1.0);
+	debugText_->Print(strDebug2, 50, 65, 1.0);
+	debugText_->Print(strDebug3, 50, 80, 1.0);
 }
 
 void GameScene::Draw() {
